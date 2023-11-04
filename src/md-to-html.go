6bd13f9ca0a9ea8386fcf03b017a1da494cc0331
sekichii <133399213+sekichii@users.noreply.github.com>
2023-11-04 11:17:56 +0100

    idk anymore i just need to learn more go

--- src/md-to-html.go
+++ src/md-to-html.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -12,12 +13,54 @@ func check(e error) {
 	}
 }
 
+func convert(md string) string {
+	lines := strings.Split(md, "\n")
+	line_index := 0
+	html := ""
+
+	for true { //while true
+		if strings.Contains(lines[line_index], "#") {
+			header_count := strings.Count(lines[line_index], "#")
+			h1, h2 := fmt.Sprintf("<h%d>", header_count), fmt.Sprintf("</h%d>", header_count)
+			html += h1 + strings.TrimLeft(lines[line_index], "#") + h2 + "\n"
+		} else if strings.Contains(lines[line_index], "-") {
+			html += "<ul>\n"
+			for true {
+				if strings.Contains(lines[line_index], "-") {
+					html += "<li>" + strings.TrimLeft(lines[line_index], "-") + "</li>\n"
+				} else {
+					break
+				}
+				line_index++
+			}
+			html += "</ul>\n"
+		}
+
+	}
+	return html
+}
+
 func main() {
+	document_start :=
+		`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+		</head>
+		<body>`
+
+	document_end :=
+		`</body>
+		</html>`
+
 	filepath := os.Args[1]
 
 	contents, err := os.ReadFile(filepath)
 	check(err)
-	fmt.Println(string(contents))
+	html := document_start + convert(string(contents)) + document_end
+
+	f, err := os.Create("output/index.html")
+	check(err)
+	f.WriteString(html)
 
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
 }
